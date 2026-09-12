f32fbff88125e891783e9a926a2c4ee27613a991
RAPTORdebug <sihathsethmika@gmail.com>
2026-09-12 13:15:58 +0530

    logger and exception

---
+++ mlproject/src/exception.py
@@ -0,0 +1,27 @@
+import sys
+
+def error_message_detail(error, error_detail:sys):
+
+    '''
+    gives the error in a specifc manner respect to the custom exception
+    '''
+
+    _,_,exc_tb = error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    error_message = "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
+        file_name,exc_tb.tb_lineno,str(error)
+    )
+
+    return error_message
+
+class CustomException(Exception):
+    def __int__(self, error_message, error_detail:sys):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message, error_detail=error_detail)
+
+    def __str__(self):
+        '''
+        when u raise the error this will show the error
+        '''
+        return self.error_message
+
