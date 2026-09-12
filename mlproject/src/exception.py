ad98c77425f1cb22db9690e740780d4395ec97d4
RAPTORdebug <sihathsethmika@gmail.com>
2026-09-12 22:21:31 +0530

    added eda and model training notebooks

--- mlproject/src/exception.py
+++ mlproject/src/exception.py
@@ -1,4 +1,5 @@
 import sys
+from src.logger import logging
 
 def error_message_detail(error, error_detail:sys):
 
@@ -15,9 +16,10 @@ def error_message_detail(error, error_detail:sys):
     return error_message
 
 class CustomException(Exception):
-    def __int__(self, error_message, error_detail:sys):
+    def __init__(self, error_message, error_detail:sys):
         super().__init__(error_message)
         self.error_message = error_message_detail(error_message, error_detail=error_detail)
+        logging.error(self.error_message)
 
     def __str__(self):
         '''
